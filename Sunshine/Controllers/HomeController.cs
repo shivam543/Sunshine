546d715db40c0e0af8a34dbdbe112d850d7d5c64
iamshivampatel12@gmail.com <iamshivampatel12@gmail.com>
2024-03-09 11:07:55 +0530

    Add project files.

---
+++ Sunshine/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sunshine.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult air_cargo()
+        {
+            return View();
+        }
+        public ActionResult air_cargo_agents()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+        }
+        public ActionResult commercial_shipment()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult courier_services()
+        {
+            return View();
+        }
+        public ActionResult document_and_parcel()
+        {
+            return View();
+        }
+        public ActionResult excess_baggage_delivery()
+        {
+            return View();
+        }
+        public ActionResult express_import_services()
+        {
+            return View();
+        }
+        public ActionResult food_items_delivery()
+        {
+            return View();
+        }
+        public ActionResult gallery()
+        {
+            return View();
+        }
+        public ActionResult household_items()
+        {
+            return View();
+        }
+        public ActionResult international_courier_services()
+        {
+            return View();
+        }
+        public ActionResult medicine_delivery()
+        {
+            return View();
+        }
+        public ActionResult reasons_why_you_should_use_courier_services()
+        {
+            return View();
+        }
+        public ActionResult sea_cargo()
+        {
+            return View();
+        }
+        public ActionResult tips_to_choose_best_courier_service()
+        {
+            return View();
+        }
+        public ActionResult types_of_courier_services()
+        {
+            return View();
+        }
+    }
+}
